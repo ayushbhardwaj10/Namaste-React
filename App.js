093494311341048071485503e50755e8bef4d340
Ayush Bhardwaj <ayush.b.1998@gmail.com>
2023-08-06 12:36:58 -0700

    Temporary commit : Episode 03

--- App.js
+++ App.js
@@ -1,11 +1,2 @@
-// const heading = React.createElement("h1", { id: "heading" }, "Hello from ReactJS");
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "I'm h1 tag"), React.createElement("h2", {}, "I'm h2 tag")])
-);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
