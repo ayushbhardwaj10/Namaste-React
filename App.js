d1bc0e851dc37968d652a67ce4c01f36f91827cd
Ayush Bhardwaj <ayush.b.1998@gmail.com>
2023-08-06 22:00:59 -0700

    Episode 03 (With Notes)

--- App.js
+++ App.js
@@ -1,2 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Namaste React using JSX
+  </h1>
+);
+
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1 className="heading">Namaste React Function Component</h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />);
