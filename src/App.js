ca652e09759ca9b616c0ecee6f699ceca8a28c54
Ayush Bhardwaj <40769053+ayushbhardwaj10@users.noreply.github.com>
2023-09-15 15:43:16 -0700

    Episode 12 (With Notes) - Redux - Partial episode 1:12:24

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu.js";
 import UserContext from "./utils/UserContext.js";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore.js";
 const Grocery = lazy(() => import("./components/Grocery.js"));
 
 // Parent Component where all the components are rendered
@@ -23,19 +25,21 @@ const AppLayout = () => {
   }, []);
 
   return (
-    <UserContext.Provider
-      value={{
-        loggedInUser: userName,
-        updateUserName: (name) => {
-          setUserName(name);
-        },
-      }}
-    >
-      <div className="app">
-        <Header />
-        <Outlet />
-      </div>
-    </UserContext.Provider>
+    <Provider store={appStore}>
+      <UserContext.Provider
+        value={{
+          loggedInUser: userName,
+          updateUserName: (name) => {
+            setUserName(name);
+          },
+        }}
+      >
+        <div className="app">
+          <Header />
+          <Outlet />
+        </div>
+      </UserContext.Provider>
+    </Provider>
   );
 };
 const appRouter = createBrowserRouter([
